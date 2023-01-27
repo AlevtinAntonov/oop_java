4345d97606dfdfb5f47b58c6c3dbfbaca851292e
Alevtin <antonov3017@gmail.com>
2023-01-27 16:56:29 +0300

    Lesson_11 add some corr

--- src/Lesson_11/Ex002Phonebook/Config.java
+++ src/Lesson_11/Ex002Phonebook/Config.java
@@ -1,5 +1,5 @@
 package Lesson_11.Ex002Phonebook;
 
 public class Config {
-    public static String pathDb = "data.db";
+    public static String pathDb = "src/Lesson_11/Ex002Phonebook/data.db";
 }
